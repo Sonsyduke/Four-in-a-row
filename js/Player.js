f308eb72f0fe05c4583d36398cd0c215cdabf69d
Douglas <douglaslombard29@gmail.com>
2022-04-08 13:45:54 +0200

    created player and token classes

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,24 @@
-class Player {}
+class Player {
+  constructor(name, id, color, active = false) {
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = this.createTokens(21);
+  }
+
+  /**
+   * Creates token objects for the player
+   * @param {integer} num - Number of token objects to be created
+   * @returns {array} tokens - an array of new token objects for a player
+   */
+  createTokens(num) {
+    const tokens = [];
+
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+    return tokens;
+  }
+}

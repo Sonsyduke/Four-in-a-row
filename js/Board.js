83aef9a1ad39e4a0155f41e315413152afb3f76a
Douglas <douglaslombard29@gmail.com>
2022-04-09 17:18:16 +0200

    added all the constructor methods

--- js/Board.js
+++ js/Board.js
@@ -2,6 +2,28 @@ class Board {
   constructor() {
     this.rows = 6;
     this.columns = 7;
-    this.spaces = [];
+    this.spaces = this.createSpaces();
+  }
+
+  /**
+   * Generates 2D array of spaces.
+   * @returns {array} An array of space objects
+   */
+
+  createSpaces() {
+    const spaces = [];
+
+    for (let i = 0; i < this.columns; i++) {
+      const column = [];
+
+      for (let y = 0; y < this.rows; y++) {
+        const space = new Space(i, y);
+        column.push(space);
+      }
+
+      spaces.push(column);
+    }
+
+    return spaces;
   }
 }
